1896ac377ed1353719ce8783da429c6615402e7b
Viktor Kosenko <kosenkovv89@gmail.com>
2020-08-07 13:57:54 +0300

    практика 2

--- js/script.js
+++ js/script.js
@@ -10,14 +10,42 @@ const personalMovieDB = {
     privat: false
 };
 
-const nameMovies1 = prompt("Один из последних просмотренных фильмов?", "Тест"),
-    balMovies1 = prompt("На сколько оцените его?", "9.1"),
-    nameMovies2 = prompt("Один из последних просмотренных фильмов?", "Тест"),
-    balMovies2 = prompt("На сколько оцените его?", "9.1");
 
-personalMovieDB.movies = {
-    [nameMovies1]: balMovies1,
-    [nameMovies2]: balMovies2
-};
+// let i = 0;
+// do {
+//     nameMovies = prompt("Один из последних просмотренных фильмов?", "Тест");
+//     if (nameMovies.length != 0 & nameMovies.length <= 50) {
+//         balMovies = prompt("На сколько оцените его?", "9.1");
+
+//         if (balMovies.length != 0 & balMovies.length <= 50) {
+//             personalMovieDB.movies[nameMovies] = balMovies;
+//             i++;
+//         }
+//     }
+// } while (i < 2);
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", "Тест"),
+        b = prompt("На сколько оцените его?", "9.1");
+    if (a != null & b != null) {
+        if (a != '' & b != '' & a.length < 50) {
+            personalMovieDB.movies[a] = b;
+        } else {
+            i--;
+        }
+    } else {
+        i--;
+    }
+}
+
+if (personalMovieDB.count <= 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count > 10 & personalMovieDB.count <= 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
---
+++ js/script2.js
@@ -0,0 +1,46 @@
+"use strict";
+
+if (4 == 9) {
+    console.log('Ok!');
+} else {
+    console.log('Error');
+}
+
+let num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Ok!');
+// }
+
+// (num === 50) ? console.log('Ok!'): console.log('Error');
+
+switch (num) {
+    case 49:
+        console.log('Yes');
+        break;
+    default:
+        console.log('No!');
+}
+
+while (num < 55) {
+    console.log(num);
+    num++;
+}
+
+do {
+    console.log(num);
+    num--;
+}
+while (num > 50);
+
+for (let i = 1; i < 8; i++) {
+        if (i === 6){
+      //  break;
+      continue;
+    }
+    console.log(i);
+}
