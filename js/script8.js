c2ca3078bbab77aef16d0bd5f50ad8129ae53c85
Viktor Kosenko <kosenkovv89@gmail.com>
2020-08-10 18:03:14 +0300

    2.22

--- js/script8.js
+++ js/script8.js
@@ -46,3 +46,52 @@ newNumbers.b = 10;
 
 console.log(numders);
 console.log(newNumbers);
+
+const add = {
+    d: 17,
+    e: 20
+};
+
+const clone = Object.assign({}, add);
+
+clone.d = 20;
+
+console.log(add);
+console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'asd';
+
+console.log(oldArray);
+console.log(newArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+    blogs = ['wordpress', 'livejournal', 'blogger'],
+    internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [2, 5, 7];
+
+log(...num);
+
+const array = ["a", "b"];
+
+const newArray2 = [...array]; //ES7
+
+const q = {
+    one: 1,
+    two: 2
+};
+
+const newObj = {...q}; //ES9
+
+console.log(newObj);
