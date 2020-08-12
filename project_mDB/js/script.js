02a68cc7880c5294675704544826d66097eb0868
Viktor Kosenko <kosenkovv89@gmail.com>
2020-08-12 18:01:43 +0300

    2.30 - Ok

--- project_mDB/js/script.js
+++ project_mDB/js/script.js
@@ -24,3 +24,27 @@ const movieDB = {
     ]
 };
 
+const promoAdvTitle = document.querySelectorAll('.promo__adv img'),
+    promoBg = document.querySelector('.promo__bg'),
+    promoGenre = promoBg.querySelector('.promo__genre'),
+    promoInteractiveList = document.querySelector('.promo__interactive-list');
+
+promoAdvTitle.forEach(item => {
+    item.remove();
+});
+//1
+promoGenre.textContent = "драма";
+//2
+promoBg.style.backgroundImage = "url('img/bg.jpg')";
+//3
+promoInteractiveList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((item, i) => {
+    promoInteractiveList.innerHTML += `
+    <li class="promo__interactive-item">${i + 1}. ${item}
+        <div class="delete"></div>
+    </li>
+    `;
+});
