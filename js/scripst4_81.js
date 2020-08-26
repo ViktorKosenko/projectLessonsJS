746f9be28ad3dbee1a8b757df04f70f4372d58e6
Viktor Kosenko <kosenkovv89@gmail.com>
2020-08-26 13:17:26 +0300

    4_81

---
+++ js/scripst4_81.js
@@ -0,0 +1,13 @@
+'use strict';
+
+console.log(1);
+
+setTimeout(() => {
+    console.log('timeout_2000');
+}, 2000);
+
+setTimeout(() => {
+    console.log('timeout_4000');
+}, 4000);
+
+console.log(2);
